812c90098adcb1677347ff4c56e2b6c5aca1a4c9
cxy <cxy963327970@gamil.com>
2023-04-21 13:09:09 +0900

    服务器配置

--- install_tool.sh
+++ install_tool.sh
@@ -8,4 +8,10 @@ apt install -y git vim openssh-server
 echo "root:1234" | chpasswd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 service ssh restart
-
+#编辑Jupyter配置文件以允许远程访问
+pip install jupyter
+jupyter notebook --generate-config --allow-root
+jupyter notebook password
+echo "c.NotebookApp.allow_root = True" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
--- jupyter_set.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#编辑Jupyter配置文件以允许远程访问
-pip install jupyter
-jupyter notebook --generate-config --allow-root
-jupyter notebook password
-echo "c.NotebookApp.allow_root = True" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
