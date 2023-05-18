9b91e8ceda487bb942a0bb29394c7305f6ed78c9
cxy <cxy963327970@gamil.com>
2023-05-18 14:24:50 +0900

    服务器配置

--- install_tool.sh
+++ install_tool.sh
@@ -6,7 +6,7 @@ apt install -y git vim openssh-server
 
 # 允许 SSH 远程连接
 echo "root:1234" | chpasswd
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's\/#PermitRootLogin prohibit-password\/PermitRootLogin yes\/' /etc/ssh/sshd_config
 service ssh restart
 
 #编辑Jupyter配置文件以允许远程访问
