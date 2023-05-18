cbc38edc0987254f28ad00e2eeaaf04c06eedf97
cxy <cxy963327970@gamil.com>
2023-05-18 14:27:08 +0900

    服务器配置

--- install_tool.sh
+++ install_tool.sh
@@ -6,13 +6,14 @@ apt install -y git vim openssh-server
 
 # 允许 SSH 远程连接
 echo "root:1234" | chpasswd
-sed -i 's\/#PermitRootLogin prohibit-password\/PermitRootLogin yes\/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 service ssh restart
 
-#编辑Jupyter配置文件以允许远程访问
+# 编辑 Jupyter 配置文件以允许远程访问
 pip install jupyter
 jupyter notebook --generate-config --allow-root
 jupyter notebook password
 echo "c.NotebookApp.allow_root = True" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+
